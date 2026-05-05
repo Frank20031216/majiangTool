fe7afb23790cd3dcdaab9132169cc3cc5aadfbe6
user5598866682 <3687108934907672-user5598866682@noreply.coze.cn>
2026-05-05 13:24:19 +0800

    Restored to 'de87fc1a6264fc5a6d9f49b0c55bb68dd089611d'
    Coze-Restored-To: 'de87fc1a6264fc5a6d9f49b0c55bb68dd089611d'
    Coze-Commit-Type: restore

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -42,7 +42,6 @@ export default function Index() {
   
   useEffect(() => {
     loadData()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   
   const loadData = async () => {
